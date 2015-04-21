235c6129c86f0197a353d6d1c877e7c58b730f78
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-04-21 18:44:17 -0400

    [apptags] Added apptag support to dd-agent
    The agent now sends one "dd_check:appname" tag to dogweb for each
    running check (including the JMX checks) if the user enables the
    create_dd_check_tags option. This enables slice/dice by app in the
    backend. The apptags are sent at regular intervals (every ten minutes by
    default)
    
    The running JMX apps list is retrieved from the {tmp}/jmx_status.yaml
    file. Indeed, these tags have to be sent from the collectors side, along
    with host-tags and this file happens to be the only communication
    channel between JMXFetch.jar and the agent.
    
    A test had been added to our test suite specifically for this new
    feature (checking that the apptag is sent).

--- tests/core/test_common.py
+++ tests/core/test_common.py
@@ -157,6 +157,7 @@ class TestCore(unittest.TestCase):
             'check_timings': True,
             'collect_ec2_tags': True,
             'collect_instance_metadata': False,
+            'create_dd_check_tags': False,
             'version': 'test',
             'tags': '',
         }
@@ -184,6 +185,38 @@ class TestCore(unittest.TestCase):
         for check in checks:
             tag = "check:%s" % check.name
             assert tag in all_tags, all_tags
+
+
+    @attr(requires='sysstat')
+    def test_apptags(self):
+        '''
+        Tests that the app tags are sent if specified so 
+        '''
+        agentConfig = {
+            'api_key': 'test_apikey',
+            'collect_ec2_tags': False,
+            'collect_instance_metadata': False,
+            'create_dd_check_tags': True,
+            'version': 'test',
+            'tags': '',
+        }
+
+        # Run a single checks.d check as part of the collector.
+        redis_config = {
+            "init_config": {},
+            "instances": [{"host": "localhost", "port": 6379}]
+        }
+        checks = [load_check('redisdb', redis_config, agentConfig)]
+
+        c = Collector(agentConfig, [], {}, get_hostname(agentConfig))
+        payload = c.run({
+            'initialized_checks': checks,
+            'init_failed_checks': {}
+        })
+
+        # We check that the redis DD_CHECK_TAG is sent in the payload
+        self.assertTrue('dd_check:redisdb' in payload['host-tags']['system'])
+
 
     def test_no_proxy(self):
         """ Starting with Agent 5.0.0, there should always be a local forwarder
